6b89a9f691aee56e1db76e9574b14b4bb437d5be
Dimitri <dev1@monabee.fr>
2021-01-17 15:32:25 +0100

    correction bug captcha envoi mail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,15 +33,14 @@ class HomeController extends Controller
             'required' => 'Le champs :attribute est obligatoire. ',
             'email'    => "L'email saisi n'est pas une adresse email valide. <br>",
             'g-recaptcha-response.required' => 'Etes-vous un robot ? ',
-//            'g-recaptcha-response.captcha' => "Erreur de captcha. "
+            'g-recaptcha-response.captcha' => "Erreur de captcha. "
         ];
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'body_message' => 'required',
-//            'g-recaptcha-response' => 'required|captcha'
-            'g-recaptcha-response' => 'required'
+            'g-recaptcha-response' => 'required|captcha'
         ], $customMessages);
         if ($validator->fails()) {
             return Response::json(array('success'=>false, 'errors'=>$validator->errors()));
@@ -49,7 +48,7 @@ class HomeController extends Controller
 
         Contact::create($request->all());
 
-        if(!Mail::send(['html' => 'emails.contact'],
+        Mail::send(['html' => 'emails.contact'],
             array(
                 'name' => $request->get('name'),
                 'email' => $request->get('email'),
@@ -57,10 +56,12 @@ class HomeController extends Controller
                 'body_message' => nl2br($request->get('body_message'))
             ), function($message) use ($request)
             {
-                $message->from("contact@dimitrisandron.fr");
+                $message->from("contact@dimitrisandron.fr", 'Portfolio');
                 $message->to('dimitri.sandron@outlook.fr', 'Dimitri Sandron');
-                $message->subject('Email de contact');
-            })){
+                $message->subject('Nouveau message depuis le formulaire');
+            });
+
+        if(count(Mail::failures()) > 0){
             return Response::json(array('success'=>false, 'errors'=>"Erreur lors de l'envoi."));
         }
 
